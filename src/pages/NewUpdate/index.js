09611614455466d41a43859266d168331111c23a
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-19 12:37:02 -0300

    19° commit - tdd tela NewUpdate: Existe input1 em NewUpdate? GREEN

--- src/pages/NewUpdate/index.js
+++ src/pages/NewUpdate/index.js
@@ -15,7 +15,16 @@ export default function NewUpdate(){
         <Link data-testid="mylink" className="btn btn-dark" 
         style={{marginBottom: '5px'}} to="/">Home</Link>
 
-        <form data-testid="myform"></form>
+        <form data-testid="myform">
+
+          <div className="form-group">
+            <label htmlFor="###">###</label>
+            <input data-testid="input1" id="###" type="text" 
+            style={{marginBottom: '20px'}} className="form-control" 
+            placeholder="###"></input>
+          </div>
+
+        </form>
 
       </div>
     </div>
