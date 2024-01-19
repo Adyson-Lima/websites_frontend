308e98f3c8a5ed663f2ec0d69d3aae43869e91ac
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-19 12:40:06 -0300

    21° commit - tdd tela NewUpdate: Existe input2 em NewUpdate? GREEN

--- src/pages/NewUpdate/index.js
+++ src/pages/NewUpdate/index.js
@@ -24,6 +24,13 @@ export default function NewUpdate(){
             placeholder="###"></input>
           </div>
 
+          <div className="form-group">
+            <label htmlFor="###">###</label>
+            <input data-testid="input2" id="###" type="text" 
+            style={{marginBottom: '20px'}} className="form-control" 
+            placeholder="###"></input>
+          </div>
+
         </form>
 
       </div>
