5d487e022bc95be57e2b6a9469094be00e879899
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-19 12:38:52 -0300

    20° commit - tdd tela NewUpdate: Existe input2 em NewUpdate? RED

--- src/tests/NewUpdate.test.js
+++ src/tests/NewUpdate.test.js
@@ -28,4 +28,8 @@ describe('Testes da tela NewUpdate', () => {
     expect(screen.getByTestId('input1')).toBeInTheDocument();
   });
 
+  it('Existe input2 em NewUpdate?', () => {
+    expect(screen.getByTestId('input2')).toBeInTheDocument();
+  });
+
 });
