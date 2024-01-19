8bcc66b595d4ec0660ce38ae8cbfb2b8a41141eb
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-19 12:02:30 -0300

    3° commit - tdd tela Websites: Existe card em Websites? GREEN

---
+++ src/pages/Websites/index.js
@@ -0,0 +1,20 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+// import api from '../../services/api';
+
+export default function Websites(){
+
+  return(
+
+    <div data-testid="mycard" className="card border-primary" style={{marginTop: '20px'}} >
+      <div className="card-header bg-primary" style={{color: '#fff'}}>
+        Websites Crud
+      </div>
+      <div className="card-body">
+
+      </div>
+    </div>
+
+  );
+
+}
