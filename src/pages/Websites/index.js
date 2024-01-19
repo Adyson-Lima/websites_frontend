0499b1cedc96c1925267e25bc198fb4ebc206e1e
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-19 12:56:27 -0300

    25° commit - logica update implementada

--- src/pages/Websites/index.js
+++ src/pages/Websites/index.js
@@ -13,6 +13,15 @@ export default function Websites(){
     .then(response => {setWebsites(response.data)})
   }, []);
 
+  // update, navega para tela NewUpdate
+  async function updateWebsite(id){
+    try {
+      navigate(`/newupdate/${id}`);
+    } catch (error) {
+      alert('erro ao navegar');      
+    }
+  }
+
   return(
 
     <div data-testid="mycard" className="card border-primary" style={{marginTop: '20px'}} >
@@ -42,7 +51,8 @@ export default function Websites(){
                   <td>
 
                     <button data-testid="mybtn1" type="button"
-                    className="btn btn-outline-info" style={{margin: '2px'}}>Editar</button>
+                    className="btn btn-outline-info" style={{margin: '2px'}}
+                    onClick={() => updateWebsite(website.id)}>Editar</button>
 
                     <button data-testid="mybtn2" type="button"
                     className="btn btn-outline-danger" style={{margin: '2px'}}>Excluir</button>
