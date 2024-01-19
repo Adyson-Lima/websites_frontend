90816a8cd321b327951397baf1e25c13bac8c597
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-19 12:04:19 -0300

    4° commit - tdd tela Websites: Existe link Novo em Websites? RED

--- src/tests/Websites.test.js
+++ src/tests/Websites.test.js
@@ -16,4 +16,8 @@ describe('Testes da tela Websites', () => {
     expect(screen.getByTestId('mycard')).toBeInTheDocument();
   });
 
+  it('Existe link Novo em Websites?', () => {
+    expect(screen.getByTestId('mylink')).toBeInTheDocument();
+  });
+
 });
