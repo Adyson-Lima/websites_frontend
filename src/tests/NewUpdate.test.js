b29272db5ea083167dd541ccb349ea4bc5f412f8
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-19 12:31:51 -0300

    16° commit - tdd tela NewUpdate: Existe form em NewUpdate? RED

--- src/tests/NewUpdate.test.js
+++ src/tests/NewUpdate.test.js
@@ -20,4 +20,8 @@ describe('Testes da tela NewUpdate', () => {
     expect(screen.getByTestId('mylink')).toBeInTheDocument();
   });
 
+  it('Existe form em NewUpdate?', () => {
+    expect(screen.getByTestId('myform')).toBeInTheDocument();
+  });
+
 });
